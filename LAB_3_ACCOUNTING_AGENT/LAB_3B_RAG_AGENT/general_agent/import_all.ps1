1567c24c2794ee8d1b9dac9275c30ad6c7134eb8
Phupa Denphatcharangkul <earthphupa@gmail.com>
2025-09-11 18:01:46 +0700

    feat: add import and remove powershell scripts for accounting agent

---
+++ LAB_3_ACCOUNTING_AGENT/LAB_3B_RAG_AGENT/general_agent/import_all.ps1
@@ -0,0 +1,8 @@
+git lfs install
+
+gorchestrate env activate test-env
+
+$SCRIPT_DIR = Split-Path -Parent $MyInvocation.MyCommand.Definition
+
+orchestrate knowledge-bases import -f "$SCRIPT_DIR/knowledge_base/Accounting_knowledge_base.yaml"
+orchestrate agents import -f "$SCRIPT_DIR/agents/Accounting_RAG_agent.yaml"
