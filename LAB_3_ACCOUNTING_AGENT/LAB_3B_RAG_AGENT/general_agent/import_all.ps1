6df79adde10002673befdc9b1402e643e2f47d53
Sirasith Ruengsilapavilai <105077574+dragonfore4@users.noreply.github.com>
2025-09-18 15:18:59 +0700

    fix(script):  fix orchestrate typo, correct path

--- LAB_3_ACCOUNTING_AGENT/LAB_3B_RAG_AGENT/general_agent/import_all.ps1
+++ LAB_3_ACCOUNTING_AGENT/LAB_3B_RAG_AGENT/general_agent/import_all.ps1
@@ -2,9 +2,9 @@ $env:PYTHONUTF8=1
 
 git lfs install
 
-gorchestrate env activate test-env
+orchestrate env activate test-env
 
 $SCRIPT_DIR = Split-Path -Parent $MyInvocation.MyCommand.Definition
 
-orchestrate knowledge-bases import -f "$SCRIPT_DIR/knowledge_base/Accounting_knowledge_base.yaml"
+orchestrate knowledge-bases import -f "$SCRIPT_DIR/knowledge_base/accounting_knowledge_base.yaml"
 orchestrate agents import -f "$SCRIPT_DIR/agents/Accounting_RAG_agent.yaml"
